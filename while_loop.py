e02e694a989713fce3b451c817d2f8f7434e7298
Ray-Shubham <raysubham555@gmail.com>
2022-03-03 10:22:32 +0530

    modified while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -1,8 +1,8 @@
-n=int(input("Please enter number:"))
+n = int(input("Please enter number:"))
 
-sum=0
-i=1
+sum = 0
+i = 1
 while i <= n:
-    sum=sum+i
-    i=i+1
+    sum = sum + i
+    i = i + 1
 print("The sum is ", sum)
