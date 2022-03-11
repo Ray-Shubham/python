a7b25839b83a9fb2daa9233dcd1c500e17f96bb1
Ray-Shubham <99724252+Ray-Shubham@users.noreply.github.com>
2022-03-11 17:50:09 +0530

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,5 +1,8 @@
 import git
 repo = git.Repo('/var/lib/jenkins/workspace/python_git/python')
+with repo.config_reader() as git_config:
+    print(git_config.get_value('user', 'email'))
+    print(git_config.get_value('user', 'name'))
 if repo.is_dirty(untracked_files=True):
   print("Changes Detected")
   repo.git.add(all=True)
