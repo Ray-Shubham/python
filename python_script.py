290436ddf0568fd97a815f469e4576287b0ff671
Ray-Shubham <99724252+Ray-Shubham@users.noreply.github.com>
2022-03-15 10:37:23 +0530

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -5,10 +5,10 @@ for remote in repo.remotes:
     print(f'- {remote.name} {remote.url}')'''
 
 
-with repo.config_writer() as git_config:
+'''with repo.config_writer() as git_config:
     git_config.set_value('user', 'email', 'raysubham555@gmail.com')
     git_config.set_value('user', 'name', 'Ray-Shubham')
-'''with repo.config_reader() as git_config:
+with repo.config_reader() as git_config:
     print(git_config.get_value('user', 'email'))
     print(git_config.get_value('user', 'name'))'''
 if repo.is_dirty(untracked_files=True):
