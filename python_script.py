8a352b50ecf7a4de4d430008f0b070f803859227
Ray-Shubham <99724252+Ray-Shubham@users.noreply.github.com>
2022-03-15 12:30:00 +0530

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,16 +1,18 @@
 import git
 repo = git.Repo('/var/lib/jenkins/workspace/python_git/python')
+
 '''print('Remotes:')
 for remote in repo.remotes:
     print(f'- {remote.name} {remote.url}')'''
 
-
 '''with repo.config_writer() as git_config:
     git_config.set_value('user', 'email', 'raysubham555@gmail.com')
     git_config.set_value('user', 'name', 'Ray-Shubham')
+    
 with repo.config_reader() as git_config:
     print(git_config.get_value('user', 'email'))
     print(git_config.get_value('user', 'name'))'''
+
 if repo.is_dirty(untracked_files=True):
   print("Changes Detected")
   repo.git.add(all=True)
